abd754fe477513646ced5e290e3705f385d35b44
Christoph <christophraab@outlook.de>
2021-05-16 18:32:42 +0000

    adapt make config

--- make_config.py
+++ make_config.py
@@ -9,7 +9,6 @@ def boolean_string(s):
 
 def make_parser():
     parser = argparse.ArgumentParser(description='Domain Adversarial Tangent Learning')
-    parser.add_argument('--parallel',  default=False, type=boolean_string, help="Use Data Parallel - If used set cuda:0")
     parser.add_argument('--cuda', type=str, default='cuda:0', help="GPU Device ID")
     parser.add_argument('--num_workers', type=int, default=4, help="Number of data loader workers")
     parser.add_argument('--lr', type=float, default=0.001, help="Feature Extractor and Classifier LR")
@@ -18,17 +17,16 @@ def make_parser():
     parser.add_argument('--log_path', type=str, default='log/', help="Logging directory")
     parser.add_argument('--model_path', type=str, default='models/', help="Checkpoint directory")
     parser.add_argument('--dset', type=str, default='office', help="Dataset choice: office-home,image-clef,office,visda")   
-    parser.add_argument('--source_dir', type=str, default='/home/raabc/Database/domain_adaptation/Office-31/images/amazon/', help="Source dataset directory")
-    parser.add_argument('--target_dir', type=str, default='/home/raabc/Database/domain_adaptation/Office-31/images/webcam/', help="Target dataset directory")
+    parser.add_argument('--source_dir', type=str, default='Office-31/images/amazon/', help="Source dataset directory")
+    parser.add_argument('--target_dir', type=str, default='Office-31/images/webcam/', help="Target dataset directory")
     parser.add_argument('--bottleneck_dim', type=int, default=256, help="Bottleneck Dimension of Feature Extractor")
     parser.add_argument('--subspace_dim', type=int, default=128, help="Subspace Dimension for Tangent Distances")
     parser.add_argument('--num_protos', type=int, default=31, help="Number of Prototypes")
     parser.add_argument('--dlr', type=float, default=0.005, help="Discriminator LR")
     parser.add_argument('--lent', type=float, default=0.05, help="Ent Min Coeff")   
     parser.add_argument('--eval_epoch', type=int, default=5, help="Evaluation Cycle")
-    parser.add_argument('--method', type=str, default='datl', help="[datl|datl_dsbn|jatl]")
     parser.add_argument('--save_sia',default=False, type=boolean_string, help="Save Siamese Prototypes")
-    parser.add_argument('--invert',default=True, type=boolean_string, help="Save Siamese Prototypes")
+    parser.add_argument('--invert',default=True, type=boolean_string, help="Invert distances")
 
     args =  parser.parse_args()
 
